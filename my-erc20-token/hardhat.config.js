054095fd71d568fd469efc516cf4ff37a329228e
Nguyen Kha Khiem <khiemnk@192.168.10.24>
2025-02-17 23:12:07 +0700

    Create ethereum PoA with two node

--- my-erc20-token/hardhat.config.js
+++ my-erc20-token/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     pos: {
-      url: "http://127.0.0.1:8545", // URL của mạng Ethereum PoS local
+      url: "127.0.0.1:64950", // URL của mạng Ethereum PoS local
       accounts: ["68f59e235434121f440449be304302fb55e454b8c4b1e6ca69c7e26496130b76"] // Thay bằng private key của bạn
     }
   }
